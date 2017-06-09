dd8885ac0e141a56a008da8322e93ed089adad0e
soldocode <riccardo.soldini@gmail.com>
2017-06-09 06:27:20 +0000

    Updated  Projects/pBendSheet/project.js

--- Projects/pBendSheet/project.js
+++ Projects/pBendSheet/project.js
@@ -1,24 +1,31 @@
 
 function makeObject(id)
     {
-     var thk=1
+     var thk=5
      var group = new THREE.Object3D();
-     var segment = new THREE.Object3D();
-     pShape=make_rect(100,50);
-     sPath=make_rect_path(100,50);
-     segment.add(new THREE.Line(pShape.createPointsGeometry(),lineMaterial));
-     var options = {
+     group.add(makeSegment(150,15))
+     objects[id]={"element":group}
+     return group;
+    }
+
+
+function makeSegment(lenght,thk)
+    {
+        var result=new THREE.Object3D();
+        pShape=make_rect(lenght,50);
+        sPath=make_rect_path(lenght,10);
+        result.add(new THREE.Line(pShape.createPointsGeometry(),lineMaterial));
+        var options = {
                         amount: thk,
                         steps: 1,
                         bevelSegments: 0,
                         bevelSize: 0,
                         bevelThickness: 0
                       };
-     segment.add(new THREE.Mesh(new THREE.ExtrudeGeometry(pShape, options),material));
-     segment.castShadow = group.receiveShadow = true;
-     objects[id]={"element":segment}
-     return segment;
+        result.add(new THREE.Mesh(new THREE.ExtrudeGeometry(pShape, options),material));
+        return result
     }
+
 
 function update_shape()
     {
@@ -30,3 +37,9 @@ function update_shape()
 
 
 $(".value").on("change",update_shape);
+camera.position.set(0,-8000,0);
+camera.up.set(0,0,1);
+camera.updateProjectionMatrix ();
+cameraControls.noRotate=true;
+cameraControls.update();
+
