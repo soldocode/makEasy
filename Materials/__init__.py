eaeac23917c681126e86058261d4c4fff5bc8fec
soldocode <riccardo.soldini@gmail.com>
2017-10-27 19:07:19 +0200

    modified path in Materials\__init__.py

--- Materials/__init__.py
+++ Materials/__init__.py
@@ -3,7 +3,7 @@ import os
 import makEasy
 import jsonpickle
 
-path=os.path.dirname(makEasy.__file__)+'\\Materials\\material_quality.json'
+path=os.path.dirname(makEasy.__file__)+'/Materials/material_quality.json'
 f = open(path, 'r')
 data=f.read()
 f.close()
