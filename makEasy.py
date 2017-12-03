a188c2f44689acca21fc04196e80370af4ec3a4a
Riccardo Soldini <soldocode@users.noreply.github.com>
2017-12-03 12:24:57 +0100

    Added data project repr functions

--- makEasy.py
+++ makEasy.py
@@ -6,16 +6,17 @@
 #*                                                                         *
 #***************************************************************************
 
-import jsonpickle
+import jsonpickle, json
 from dxfwrite import DXFEngine as dxf
 import geoFun, math
-import sys
+import sys,os
 
 if sys.version_info[0]==3:
     import io
 else:    
     import cStringIO as io
 
+FOLDER=(os.path.dirname(__file__))
 
 jsonpickle.set_encoder_options('simplejson', indent=4)
 
@@ -31,7 +32,19 @@ class Project(object):
         self.Name = name
         self.Description = description
         self.Path =path
-
+        
+    # get project data representation    
+    def getDataRepr(self,data):
+        print(data)
+        result={}
+        f = open(FOLDER+'/Projects/'+self.Path+'/form.json', 'r')
+        prj_form=json.load(f)
+        f.close()
+        for field in prj_form['form_data']:
+            
+            result.update(getPrjDataRepr[field['class']](data[field['name']]))
+        return result
+        
 
 class Material(object):
     def __init__(self,
@@ -40,18 +53,6 @@ class Material(object):
         self.Id=Id
         self.Thickness=Thickness
 
-
-#class Position(object): ### forse non va bene qui
-#    def __init__(self,
-#                 x=0, y=0, z=0,
-#                 xrot=0, yrot=0, zrot=0):
-
-#        self.X = x
-#        self.Y = y
-#        self.Z = z
-#        self.XRot = xrot
-#        self.YRot = yrot
-#        self.ZRot = zrot
 
 
 class Item(object):
@@ -96,57 +97,6 @@ class Item(object):
 #        print jp
         return
                
-        
-#    def ExportDXF (self):
-#        output = io.StringIO()
-#        drawing = dxf.drawing('c:/item.dxf')
-#        if len(self.WorkFlow)>0:
-#            works=self.WorkFlow
-#            for work in works:
-#                if work[0].Class=='PlasmaCut':
-#                    nodes=work[1]['Nodes']
-#                    chains=work[1]['Chain']
-#                    if len(chains)>0:
-#                        for chain in chains:
-#                            for geo in chain:
-
-#                                if geo[0]=='Line':
-#                                    print ('Line')
-#                                    drawing.add(dxf.line((nodes[geo[1]]['X'],
-#                                                          nodes[geo[1]]['Y']),
-#                                                         (nodes[geo[2]]['X'],
-#                                                          nodes[geo[2]]['Y'])))
-#                                elif geo[0]=='Arc':
-#                                    print('arc')
-#                                    arcgen=geoFun.CircleFrom3Points([nodes[geo[1]]['X'],
-#                                                                     nodes[geo[1]]['Y']],
-#                                                                    [nodes[geo[3]]['X'],
-#                                                                     nodes[geo[3]]['Y']],
-#                                                                    [nodes[geo[2]]['X'],
-#                                                                     nodes[geo[2]]['Y']])
-
-#                                    if arcgen['Direction']>0:
-#                                       drawing.add(dxf.arc(arcgen['Radius'],
- #                                                          (arcgen['Center'][0],arcgen['Center'][1]),
-#                                                            math.degrees(arcgen['P3Degree']),
-#                                                            math.degrees(arcgen['P1Degree'])))
-#                                    else:
-#                                        drawing.add(dxf.arc(arcgen['Radius'],
-#                                                           (arcgen['Center'][0],arcgen['Center'][1]),
-#                                                            math.degrees(arcgen['P1Degree']),
-#                                                            math.degrees(arcgen['P3Degree'])))
-
-
-
-#                                elif geo[0]=='Circle':
-#                                    print('circle')
-#                                    radius=nodes[geo[2]]['X']-nodes[geo[1]]['X']
-#                                    drawing.add(dxf.circle(radius,(nodes[geo[1]]['X'], nodes[geo[1]]['Y'])))
-
-#        drawing.save_to_fileobj(output)
-#        dxf_result=[output.getvalue()]
-
-#        return dxf_result
 
 
 class WorkStep(object):
@@ -235,6 +185,23 @@ class Machine(object):
     def getParameters(self,material):
         return {}
 
+
+def _getPrjNumber(v):
+    return v
+
+def _getPrjMultipleSubForm(v):
+    return v
+
+def _getPrjSheetMaterial(v):
+    return v
+
+def _getPrjHole(v):
+    return v
+
+getPrjDataRepr={'number':_getPrjNumber,
+                'multiple-subform':_getPrjMultipleSubForm,
+                'sheet_material':_getPrjSheetMaterial,
+                'hole':_getPrjHole}
 
 projectLibrary={}
 WORKSET = {}
