e3b83a95ed56701a84171e9e47fa2f4764ddb34c
Riccardo Soldini <soldocode@users.noreply.github.com>
2020-12-30 18:04:34 +0100

    MAC032 updated

--- Machines/MAC032/__init__.py
+++ Machines/MAC032/__init__.py
@@ -1,19 +1,26 @@
 import makEasy
-import types
+import types,json
+import meTime as mT
+import math
+from pathlib import Path
 
 Machine=makEasy.Machine("MAC032","Impianto Taglio Plasma - BBS")
-makEasy.WORKSET['PlasmaCut'].Machines.append(Machine)
+makEasy.WORKSET['PlasmaCut'].Machines.append(Machine)# ??????
 
-Machine.MacProperties= {
-    "XMax": 6000,
-    "YMax": 2500,
-    "HPrices":{"Load":23.00,
-               "Tool":23.00,
-               "Move":23.00,
-               "Work":40.00,
-               "Look":23.00,
-               "DwLd":23.00}
-  }
+f = open(Path(makEasy.FOLDER+"/Machines/MAC032")/"MaterialParameters.json", "r")
+MAC_PARAMETERS=json.loads(f.read())
+f.close()
+
+#Machine.MacProperties= {
+#    "XMax": 6000,
+#    "YMax": 2500,
+#    "HPrices":{"Load":23.00,
+#               "Tool":23.00,
+#               "Move":23.00,
+#               "Work":40.00,
+#               "Look":23.00,
+#               "DwLd":23.00}
+#  }
 
 Machine.Parameters={
         "Load":{
@@ -64,5 +71,52 @@ def getParameters(self,work_parameters):
     return result
 
 
+def newMacWorkTime(self,work_parameters):
+    wt=mT.WorkTime()
+    wt.Load.HourlyCost=20.00
+    wt.Tool.HourlyCost=20.00
+    wt.Move.HourlyCost=30.00
+    wt.Work.HourlyCost=50.00
+    wt.Look.HourlyCost=20.00
+    wt.Dwld.HourlyCost=20.00
+    material=work_parameters['sheet_mat']
+    thk=work_parameters['sheet_thk']
+    max_area=6.0*2.5
+
+    #load
+    w=work_parameters['total_area']*7.9*thk
+    pa=work_parameters['total_area']/max_area
+    wt.Load.TimeParameters=mT.TimeParameters(BlockTime=3+2*math.log(w+1))
+    print('load',wt.Load.TimeParameters)
+
+    #tool
+    wt.Tool.TimeParameters=mT.TimeParameters(BlockTime=0.5)
+    print('tool',wt.Tool.TimeParameters)
+
+    #move
+    wt.Move.TimeParameters=mT.TimeParameters(BlockTime=0.05,Speed=8000.0)
+    print('move',wt.Move.TimeParameters)
+
+    #work
+    mp=MAC_PARAMETERS[material][str(thk)]
+    #mp=lp[list(lp.keys())[0]]
+    #print(mp)
+    wt.Work.TimeParameters=mT.TimeParameters(BlockTime=0.16,
+                                             Speed=mp['Speed'])
+
+    print('work',wt.Work.TimeParameters)
+
+    #look
+    wt.Look.TimeParameters=mT.TimeParameters(BlockTime=2)
+    #print('look',wt.Look.TimeParameters)
+
+    #dwld
+    wt.Dwld.TimeParameters=mT.TimeParameters(BlockTime=0.3,Speed=1/thk)## non va bene!!!
+    print('dwld',wt.Dwld.TimeParameters)
+
+    return wt
+
+
 Machine.getParameters = types.MethodType( getParameters, Machine )
+Machine.newMacWorkTime = types.MethodType( newMacWorkTime, Machine )
 makEasy.MACHINES[Machine.Id]= Machine
